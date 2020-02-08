818f773be3f34e6ec9e0a6c9423078061b271c06
Mr Sekh Afrid <abhijeetmurmu97@gmail.com>
2020-02-08 21:00:49 +0530

    Updates

---
+++ precache-manifest.917cb9bdbb21aa8da8d8ac3112078ef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae5a550ccef027d7f5a8b5c7807b151",
+    "url": "/codingNinjaApi/index.html"
+  },
+  {
+    "revision": "55a052426a46ce1dcc6a",
+    "url": "/codingNinjaApi/static/css/main.cce4dd3f.chunk.css"
+  },
+  {
+    "revision": "e852f5d8035816bc3e69",
+    "url": "/codingNinjaApi/static/js/2.51c7ee2d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/codingNinjaApi/static/js/2.51c7ee2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a052426a46ce1dcc6a",
+    "url": "/codingNinjaApi/static/js/main.8bdbfe19.chunk.js"
+  },
+  {
+    "revision": "8ae71f6606f5c84439fb",
+    "url": "/codingNinjaApi/static/js/runtime-main.218ea967.js"
+  }
+]);
